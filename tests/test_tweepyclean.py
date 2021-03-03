69faec8434f3d2dcdf92ec9a8f8d19da17b59d99
Syad Khan <syad@student.ubc.ca>
2021-03-03 18:55:54 -0500

    Create structure for test function, group work

--- tests/test_tweepyclean.py
+++ tests/test_tweepyclean.py
@@ -3,3 +3,26 @@ from tweepyclean import tweepyclean
 
 def test_version():
     assert __version__ == '0.1.0'
+    
+def test_sentiment_total():
+    output = tweepyclean.sentiment_total()
+    empty_df = pd.DataFrame()
+    assert (isinstance(output, pd.DataFrame))
+    assert (len(output.columns) == 3)
+    assert (output.iloc[:, 2].sum() == output.iloc[:, 1].mean())
+    assert (sentiment_total(, ), )
+
+def test_input_sentiment_total():
+    a = [1,2,3]
+    with raises(TypeError):
+        tweepyclean.sentiment_total(a)
+    
+def test_engagement_by_hour():
+    output = tweepyclean.engagement_by_hour()
+    assert (isinstance(output, plotly.graph_objs._figure.Figure))
+    
+def test_input_type_engagement():
+    a = [1,2,3]
+    with raises(TypeError):
+        tweepyclean.engagement_by_hour(a)
+

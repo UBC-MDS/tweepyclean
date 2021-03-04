b4f953370309ea6f4a5440695f1292abb1af0153
Syad Khan <syad@icogo.com>
2021-03-04 11:46:00 -0500

    Remove group work function to avoid errors

--- tests/test_tweepyclean.py
+++ tests/test_tweepyclean.py
@@ -16,25 +16,4 @@ def load_clean_data():
     return clean_dataframe
 
 
-def test_sentiment_total():
-    output = tweepyclean.sentiment_total()
-    empty_df = pd.DataFrame()
-    assert (isinstance(output, pd.DataFrame))
-    assert (len(output.columns) == 3)
-    assert (output.iloc[:, 2].sum() == output.iloc[:, 1].mean())
-    assert (sentiment_total(, ), )
-
-def test_input_sentiment_total():
-    a = [1,2,3]
-    with raises(TypeError):
-        tweepyclean.sentiment_total(a)
-    
-def test_engagement_by_hour():
-    output = tweepyclean.engagement_by_hour()
-    assert (isinstance(output, plotly.graph_objs._figure.Figure))
-    
-def test_input_type_engagement():
-    a = [1,2,3]
-    with raises(TypeError):
-        tweepyclean.engagement_by_hour(a)
 

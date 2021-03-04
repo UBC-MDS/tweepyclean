765140fbd3d644371aba212e7c1ab360cefe7c15
Syad Khan <syad@icogo.com>
2021-03-04 11:37:59 -0500

    Add functions to load raw and clean data to be used in our testing functions

--- tests/test_tweepyclean.py
+++ tests/test_tweepyclean.py
@@ -1,9 +1,21 @@
 from tweepyclean import __version__
 from tweepyclean import tweepyclean
+import pandas as pd
 
 def test_version():
     assert __version__ == '0.1.0'
+
+def load_raw_data():
+    """Loads raw data used for testing"""
+    raw_dataframe = pd.read_csv('raw_df_example_tweets.csv')
+    return raw_dataframe
     
+def load_clean_data():
+    """Loads clean data used for testing"""
+    clean_dataframe = pd.read_csv('clean_df_example_tweets.csv')
+    return clean_dataframe
+
+
 def test_sentiment_total():
     output = tweepyclean.sentiment_total()
     empty_df = pd.DataFrame()

d8f311a8f8c2a3d3b1ce7c768c996ffc67bfbd0e
Syad Khan <syad@icogo.com>
2021-03-04 11:58:23 -0500

    Fix filepath issue in functions that load data

--- tests/test_tweepyclean.py
+++ tests/test_tweepyclean.py
@@ -7,13 +7,17 @@ def test_version():
 
 def load_raw_data():
     """Loads raw data used for testing"""
-    raw_dataframe = pd.read_csv('raw_df_example_tweets.csv')
+    raw_dataframe = pd.read_csv('tests/raw_df_example_tweets.csv')
     return raw_dataframe
     
 def load_clean_data():
     """Loads clean data used for testing"""
-    clean_dataframe = pd.read_csv('clean_df_example_tweets.csv')
+    clean_dataframe = pd.read_csv('tests/clean_df_example_tweets.csv')
     return clean_dataframe
 
+def test_tweet_words():
+    clean_data = load_clean_data()
+    assert(5 == 5)
+    
 
 

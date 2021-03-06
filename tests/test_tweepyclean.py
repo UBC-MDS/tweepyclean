0d6b35f925c4d1365ce197b09c1249ac4f3f4917
Matthew Pin <MattTPin@gmail.com>
2021-03-05 16:06:10 -0800

    added raw_df() and clean_df() function and tests, added neccesary dependencies

--- tests/test_tweepyclean.py
+++ tests/test_tweepyclean.py
@@ -3,6 +3,11 @@ from tweepyclean import tweepyclean
 import pandas as pd
 import altair as alt
 from pytest import raises
+import re # Needed for clean_tweets() regex to remove emojis
+from nltk.sentiment.vader import SentimentIntensityAnalyzer # Needed for clean_tweets() to generate sentiment score
+import textstat # Needed for clean_tweets() to generate flesch_readability score
+import emoji # Needed for clean_tweets() to extract emojis
+import tweepy # Needed to check for tweepy.cursor.ItemIterator object
 
 def test_version():
     assert __version__ == '0.1.0'
@@ -16,6 +21,101 @@ def load_clean_data():
     """Loads clean data used for testing"""
     clean_dataframe = pd.read_csv('tests/clean_df_example_tweets.csv')
     return clean_dataframe
+
+def test_raw_df():
+    "tests raw_df() function"
+    with raises(TypeError):
+        tweepyclean.raw_df("wrong_datatype")
+
+def test_clean_tweets():
+    "tests clean_tweets() function"
+    raw_df = load_raw_data()
+    output_df_test = tweepyclean.clean_tweets(raw_df, handle = "Canucks")
+    
+    # Confirm output is a dataframe
+    assert (isinstance(tweepyclean.clean_tweets(raw_df, handle = "Canucks"), pd.DataFrame))
+    
+    # Check input type errors
+    with raises(TypeError):
+        tweepyclean.clean_tweets(["wrong", "datatype"], handle = "Canucks")
+    with raises(TypeError):
+        tweepyclean.clean_tweets(raw_df, handle = 156.56)
+    
+    # Check if neccesary columns are present
+    raw_df_no_retweet_count = raw_df.drop('retweet_count', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_retweet_count, handle = "Canucks")
+        
+    raw_df_no_favorite_count = raw_df.drop('favorite_count', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_retweet_count, handle = "Canucks")
+    
+    raw_df_no_entities = raw_df.drop('entities', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_entities, handle = "Canucks")
+    
+    raw_df_no_full_text = raw_df.drop('full_text', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_entities, handle = "Canucks")
+    
+    # Confirm output is a dataframe
+    assert (isinstance(tweepyclean.clean_tweets(raw_df, handle = "Canucks"), pd.DataFrame))
+    
+    # Check input type errors
+    with raises(TypeError):
+        tweepyclean.clean_tweets(["wrong", "datatype"], handle = "Canucks")
+    with raises(TypeError):
+        tweepyclean.clean_tweets(raw_df, handle = 156.56)
+    
+    # Check if neccesary columns are present
+    raw_df_no_retweet_count = raw_df.drop('retweet_count', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_retweet_count, handle = "Canucks")
+        
+    raw_df_no_favorite_count = raw_df.drop('favorite_count', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_retweet_count, handle = "Canucks")
+    
+    raw_df_no_entities = raw_df.drop('entities', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_entities, handle = "Canucks")
+    
+    raw_df_no_full_text = raw_df.drop('full_text', 1)
+    with raises(ValueError):
+        tweepyclean.clean_tweets(raw_df_no_entities, handle = "Canucks")
+    
+    # Check if the correct columns are generated in for output
+    output_df_test_handle = tweepyclean.clean_tweets(raw_df, handle = "Canucks")
+    assert 'handle' in output_df_test_handle.columns, "no handle column in output when it should have been generated"
+    
+    output_df_text_only = tweepyclean.clean_tweets(raw_df, handle = "Canucks", text_only = True)
+    assert 'text_only' in output_df_text_only.columns, "no text_only column in output when it should have been generated"
+    
+    output_df_word_count= tweepyclean.clean_tweets(raw_df, handle = "Canucks", word_count = True)
+    assert 'word_count' in output_df_word_count.columns, "no word_count column in output when it should have been generated"
+    
+    output_df_emojis = tweepyclean.clean_tweets(raw_df, handle = "Canucks", emojis = True)
+    assert 'emojis' in output_df_emojis.columns, "no emojis column in output when it should have been generated"
+    
+    output_df_hashtags = tweepyclean.clean_tweets(raw_df, handle = "Canucks", hashtags = True)
+    assert 'hashtags' in output_df_hashtags.columns, "no hashtags column in output when it should have been generated"
+    
+    output_df_sentiment = tweepyclean.clean_tweets(raw_df, handle = "Canucks", sentiment = True)
+    assert 'sentiment_polarity' in output_df_sentiment.columns, "no sentiment_polarity column in output when it should have been generated"
+    
+    output_df_flesch_readability = tweepyclean.clean_tweets(raw_df, handle = "Canucks", flesch_readability = True)
+    assert 'flesch_readability_score' in output_df_flesch_readability.columns, "no flesch_readability_score column in output when it should have been generated"
+    
+    # output_df_media_links = tweepyclean.clean_tweets(raw_df, handle = "Canucks", media_links = True)
+    # assert 'media' in output_df_media_links.columns, "no media column in output when it should have been generated"
+    # assert 'media_url' in output_df_media_links.columns, "no media_url column in output when it should have been generated"
+    # assert 'media_link' in output_df_media_links.columns, "no media_link column in output when it should have been generated"
+
+    output_df_proportion_of_avg_retweets = tweepyclean.clean_tweets(raw_df, handle = "Canucks", proportion_of_avg_retweets = True)
+    assert 'prptn_rts_vs_avg' in output_df_proportion_of_avg_retweets.columns, "no prptn_rts_vs_avg column in output when it should have been generated"
+    
+    output_df_proportion_of_avg_favorites = tweepyclean.clean_tweets(raw_df, handle = "Canucks", proportion_of_avg_favorites = True)
+    assert 'proportion_favorites_vs_avg' in output_df_proportion_of_avg_favorites.columns, "no proportion_favorites_vs_avg column in output when it should have been generated"
 
 
 def test_tweet_words():
